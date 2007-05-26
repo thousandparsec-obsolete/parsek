8ccef63b2fad7e0823078f398bfd085907e84765
Jure Repinc <jlp@holodeck1.com>
2007-05-26 18:15:29 +0200

    Started wok on logger

---
+++ src/loggerwidget.h
@@ -0,0 +1,40 @@
+/* loggerwidget.h
+
+  Copyright (c) 2007   Jure Repinc (jlp@holodeck1.com)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+*/
+
+#ifndef LOGGERWIDGET_H
+#define LOGGERWIDGET_H
+
+#include "ui_loggerwidget.h"
+#include <QWidget>
+#include <tpproto/logger.h>
+
+class LoggerWidget : public QWidget, public Ui::LoggerWidget,
+                     public TPProto::Logger
+{
+    Q_OBJECT
+public:
+    LoggerWidget(QWidget *parent = 0);
+    void error(char* mesg, ...);
+    void warning(char* mesg, ...);
+    void info(char* mesg, ...);
+    void debug(char* mesg, ...);
+};
+
+#endif // LOGGERWIDGET_H
