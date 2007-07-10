c2c3276aaac18d0940d8b0623ccd528f82ffb443
Jure Repinc <jlp@holodeck1.com>
2007-07-10 04:35:24 +0200

    Adapted to changes in KAboutData

--- src/parsek.cpp
+++ src/parsek.cpp
@@ -27,21 +27,23 @@
 #include "mainwindow.h"
 #include "version.h"
 
-static const char description[] = I18N_NOOP("A KDE client for playing "
-                                            "multi-player turn-based space "
-                                            "strategy games");
+static const KLocalizedString description = 
+                         ki18n("Client for playing multi-player "
+                               "turn-based space strategy games");
+
 int main(int argc, char *argv[])
 {
-    KAboutData aboutData("parsek",
-                         I18N_NOOP("Parsek"),
+    KAboutData aboutData("parsek", // program name
+                         "parsek", // translation catalog
+                         ki18n("Parsek"),
                          PARSEK_VERSION,
                          description,
                          KAboutData::License_GPL,
-                         I18N_NOOP("Copyright (c) 2006, Jure Repinc")
+                         ki18n("Copyright (c) 2006-2007, Jure Repinc")
                          );
 
-    aboutData.addAuthor("Jure Repinc",
-                        I18N_NOOP("Main developer"),
+    aboutData.addAuthor(ki18n("Jure Repinc"),
+                        ki18n("Main developer"),
                         "jlp@holodeck1.com",
                         "http://jlp.holodeck1.com/");
 
